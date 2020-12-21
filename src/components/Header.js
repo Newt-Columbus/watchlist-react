e7dba0ec0c20179cc1d130564ac92dbbd0e25674
Anton <columbus.prog@gmail.com>
2020-12-21 22:14:59 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-content">
+
+          <div className="brand">
+            <Link to="/">Список фильмов</Link>
+          </div>
+
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Хочу посмотреть</Link>
+            </li>
+
+            <li>
+              <Link to="/watched">Просмотренные</Link>
+            </li>
+
+            <li>
+              <Link to="/add" className="btn btn-main">
+                + Добавить
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
